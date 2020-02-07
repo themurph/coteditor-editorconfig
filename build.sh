08fdd4e872897ee605d84ac857d429550845a83d
Chris Murphy <themurph.public@gmail.com>
2020-02-07 16:10:43 -0500

    Add creation of 'dist' to build.sh
    Create 'dist' directory if it doesn't exist

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir dist >/dev/null 2>&1
 osacompile -l JavaScript -o dist/EditorConfig.scptd index.js
 mkdir -p dist/EditorConfig.scptd/Contents/Resources/Script\ Libraries
 curl -fsSL https://github.com/umireon/editorconfig-jxa/releases/download/v0.7.0/editorconfig-jxa.scpt -o dist/EditorConfig.scptd/Contents/Resources/Script\ Libraries/editorconfig-jxa.scpt
